efb7c3cfad74912865e923a31470bca1178e6334
IgorRud23 <rud.igor23@gmail.com>
2020-04-12 18:08:12 -0400

    create controller and model

---
+++ app/controllers/magazines_controller.rb
@@ -0,0 +1,31 @@
+class MagazinesController < ApplicationController
+
+  skip_before_action :verify_authenticity_token
+
+  def list
+    @magazines = Magazine.all
+    render json: @magazines
+  end
+
+  def create
+    @magazine = Magazine.new(magazines_params)
+      if @magazine.save
+        render json: @magazine
+      else
+        render json: {status: "error", code: 400, message: "Try again"}
+      end
+  end
+
+  def delete
+    @magazine ||= Magazine.find(params[:id])
+    @magazine.delete
+    render json: params[:id]
+  end
+
+private
+
+  def magazines_params
+    params.require(:magazine).permit(:name, :image_src, :discription, :release_date)
+  end
+  
+end
---
+++ db/migrate/20200412215844_create_magazines.rb
@@ -0,0 +1,8 @@
+class CreateMagazines < ActiveRecord::Migration[6.0]
+  def change
+    create_table :magazines do |t|
+
+      t.timestamps
+    end
+  end
+end
