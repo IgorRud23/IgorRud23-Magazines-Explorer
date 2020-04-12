4d7e53ff8721393e6e449b8a130d861e9ae34d61
IgorRud23 <rud.igor23@gmail.com>
2020-04-12 18:28:29 -0400

    add db

--- db/migrate/20200412215844_create_magazines.rb
+++ db/migrate/20200412215844_create_magazines.rb
@@ -1,6 +1,10 @@
 class CreateMagazines < ActiveRecord::Migration[6.0]
   def change
     create_table :magazines do |t|
+      t.string :name
+      t.string :image_src
+      t.string :discription
+      t.integer :release_date
 
       t.timestamps
     end
