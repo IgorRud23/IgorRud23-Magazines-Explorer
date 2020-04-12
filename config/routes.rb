4a1a5606aab084d6ef136218f33a60df6924c9e7
IgorRud23 <rud.igor23@gmail.com>
2020-04-12 18:32:26 -0400

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope '/api' do
+  get '/mazines/list' => 'magazines#list'
+  post '/magazines/create' => 'magazines#create'
+  post '/magazines/delete' => 'magazines#delete'
+end
+
 end
