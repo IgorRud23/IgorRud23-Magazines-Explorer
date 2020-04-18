72e03cd6b85b7920d96caa0083d02ac436bdd178
IgorRud23 <rud.igor23@gmail.com>
2020-04-17 22:39:59 -0400

    created store

--- client/src/redux-bank/store.js
+++ client/src/redux-bank/store.js
@@ -1,3 +1,8 @@
 import {createStore, applyMiddleware} from 'redux'; //use redux middleware to respond to and modify state change
 import promise from 'redux-promise-middleware';
 import logger from 'redux-logger';
+import appReducer from './reducers';
+
+const store = createStore(appReducer, applyMiddleware(promise, logger));
+
+export default store;
