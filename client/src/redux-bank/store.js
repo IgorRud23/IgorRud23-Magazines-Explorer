8860e0912979faf4b79c55686955f44b936a870d
IgorRud23 <rud.igor23@gmail.com>
2020-04-17 22:35:55 -0400

    seting up redux

---
+++ client/src/redux-bank/store.js
@@ -0,0 +1,3 @@
+import {createStore, applyMiddleware} from 'redux'; //use redux middleware to respond to and modify state change
+import promise from 'redux-promise-middleware';
+import logger from 'redux-logger';
