88de2d1c526de84cbce869da27b510097f20d851
IgorRud23 <rud.igor23@gmail.com>
2020-04-18 00:22:54 -0400

    init routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import {HashRouter, Switch, Route} from 'react-router-dom';
+
+import {Provider} from 'react-redux';
+import store from './redux-bank/store';
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <HashRouter>
+      <Provider store={store}>
+        <div className="App">
+          <Switch>
+            <Route exact path='/' component={() => <h1>HomePage</h1>}/>
+            <Route exact path='/create' component={() => <h1>CreateMagazine</h1>}/>
+            <Route exact path='/magazinedetails/:id' component={() => <h1>MagazineDetails</h1>}/>
+            <Route exact path='/delete' component={() => <h1>DeleteMagazine</h1>}/>
+          </Switch>
+        </div>
+      </Provider>
+    </HashRouter>
   );
 }
 
